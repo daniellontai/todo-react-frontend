49274493b61383fb91d64cb5d23afba59e63ebbc
Lontai Dániel <lontai.daniel@gmail.com>
2024-04-09 22:04:04 +0200

    API helper functions for new list routes, error message clarification. TODO: deprecate get_tasks

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,28 +1,116 @@
 const apiEndpoints = {
-    get_tasks:          "/tasks",
-    post_task:          "/task",
-    get_task_by_id:     "/tasks/",
-    patch_task_by_id:   "/task/",
-    delete_task_by_id:  "/task/" 
-}
-
-/*API interaction functions*/
-
+	get_lists: '/lists',
+	post_list: '/list',
+	get_list_tasks: '/tasks/',
+	get_tasks: '/tasks', //deprecated
+	post_task: '/task',
+	get_task_by_id: '/tasks/',
+	patch_task_by_id: '/task/',
+	delete_task_by_id: '/task/',
+};
 
 /**
- * Asynchronously fetches tasks data from the API.
+ * Asynchronously retrieves lists from the API and returns the JSON response.
+ *
+ * @return {Object} The JSON response from the API
+ */
+async function getLists() {
+	try {
+		const apiUrl = generateApiUrl('get_lists');
+		const response = await fetch(apiUrl);
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} catch (error) {
+		return {
+			error: [
+				{
+					code: error.code,
+					message:
+						'Failed to get lists from database. API call returned invalid response or application failed to generate API url. Please ensure that the server is running. [getLists]',
+				},
+			],
+		};
+	}
+}
+
+/**
+ * Retrieves a list of tasks associated with the provided list ID from the server.
+ *
+ * @param {string} listId - The ID of the list for which tasks are being retrieved
+ * @return {Promise<Object>} A Promise that resolves with the JSON response containing the list of tasks, or an error object if the request fails
+ */
+async function getListTasks(listId) {
+	try {
+		const apiUrl = generateApiUrl('get_list_tasks', listId);
+		const response = await fetch(apiUrl);
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} catch (error) {
+		return {
+			error: [
+				{
+					code: error.code,
+					message:
+						'Failed to get tasks from database. API call returned invalid response or application failed to generate API url. Please ensure that the server is running. [getListTasks]',
+				},
+			],
+		};
+	}
+}
+
+/**
+ * Asynchronously posts a list of data to the server.
+ *
+ * @param {object} listData - The data of the list to be posted
+ * @return {Promise} A promise that resolves to the JSON response from the server
+ */
+async function postList(listData) {
+	if (typeof listData !== 'object' || listData == null) {
+		throw new Error('listData error (expected object) [postList]');
+	}
+	if (listData.name == null || typeof listData.name !== 'string') {
+		throw new Error('listData.name error (expected string) [postList]');
+	}
+	try {
+		const apiUrl = generateApiUrl('post_list');
+		const response = await fetch(apiUrl, {
+			method: 'POST',
+			body: JSON.stringify(listData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} catch (error) {
+		return {
+			error: [{ code: error.code, message: 'Creating list was unsuccessful. API call returned invalid response or application failed to generate API url. [postList]' }],
+		};
+	}
+}
+
+/**
+ * Deprecated - Asynchronously fetches tasks data from the API.
  *
  * @return {Promise<object>} The JSON response containing the tasks data.
  */
 async function getTasks() {
-    try {
-        const apiUrl = generateApiUrl("get_tasks");
-        const response  = await fetch(apiUrl);
-        const jsonResponse = await response.json();
-        return jsonResponse;
-    } catch (error) {
-        return {error: [{code: error.code, message: "Failed to get tasks from database. API call returned invalid response or application failed to generate API url. Please ensure that the server is running."}]}
-    } 
+	try {
+		const apiUrl = generateApiUrl('get_tasks');
+		const response = await fetch(apiUrl);
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} catch (error) {
+		return {
+			error: [
+				{
+					code: error.code,
+					message:
+						'Failed to get tasks from database. API call returned invalid response or application failed to generate API url. Please ensure that the server is running. [getTasks]',
+				},
+			],
+		};
+	}
 }
 
 /**
@@ -32,17 +120,17 @@ async function getTasks() {
  * @return {Promise<object>} The task data as a JSON object
  */
 async function getTaskById(id) {
-    if ((!Number.isInteger(id) || id < 1)) {
-        throw new Error ("id error (expected Int n > 0)");
-    }
-    try {
-        const apiUrl = generateApiUrl("get_task_by_id", id);
-        const response  = await fetch(apiUrl);
-        const jsonResponse = await response.json();
-        return jsonResponse;
-    } catch (error) {
-        throw new Error(error);
-    } 
+	if (!Number.isInteger(id) || id < 1) {
+		throw new Error('id error (expected Int n > 0) [getTaskById]');
+	}
+	try {
+		const apiUrl = generateApiUrl('get_task_by_id', id);
+		const response = await fetch(apiUrl);
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} catch (error) {
+		throw new Error(error);
+	}
 }
 
 /**
@@ -52,23 +140,25 @@ async function getTaskById(id) {
  * @return {Promise} the JSON response from the server
  */
 async function postTask(taskData) {
-    if (typeof taskData != "object" || taskData == null ) {
-        throw new Error("taskData error (expected object)");
-    }
-    try {
-        const apiUrl = generateApiUrl("post_task");
-        const response  = await fetch(apiUrl, {
-            method: "POST",
-            body: JSON.stringify(taskData),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        const jsonResponse = await response.json();
-        return jsonResponse;
-    } catch (error) {
-        return {error: [{code: error.code, message: "Creating task was unsuccessful. API call returned invalid response or application failed to generate API url."}]}
-    } 
+	if (typeof taskData != 'object' || taskData == null) {
+		throw new Error('taskData error (expected object) [postTask]');
+	}
+	try {
+		const apiUrl = generateApiUrl('post_task');
+		const response = await fetch(apiUrl, {
+			method: 'POST',
+			body: JSON.stringify(taskData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} catch (error) {
+		return {
+			error: [{ code: error.code, message: 'Creating task was unsuccessful. API call returned invalid response or application failed to generate API url. [postTask]' }],
+		};
+	}
 }
 
 /**
@@ -79,26 +169,28 @@ async function postTask(taskData) {
  * @return {Promise<object>} A JSON object representing the updated task
  */
 async function patchTask(id, taskData) {
-    if ((!Number.isInteger(id) || id < 1)) {
-        throw new Error ("id error (expected Int n > 0)");
-    }
-    if (typeof taskData != "object" || taskData == null ) {
-        throw new Error("taskData error (expected object)");
-    }
-    try {
-        const apiUrl = generateApiUrl("patch_task_by_id", id);
-        const response  = await fetch(apiUrl, {
-            method: "PATCH",
-            body: JSON.stringify(taskData),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        const jsonResponse = await response.json();
-        return jsonResponse;
-    } catch (error) {
-        return {error: [{code: error.code, message: "Updating task was unsuccessful. API call returned invalid response or application failed to generate API url."}]}
-    } 
+	if (!Number.isInteger(id) || id < 1) {
+		throw new Error('id error (expected Int n > 0) [patchTask]');
+	}
+	if (typeof taskData != 'object' || taskData == null) {
+		throw new Error('taskData error (expected object) [patchTask]');
+	}
+	try {
+		const apiUrl = generateApiUrl('patch_task_by_id', id);
+		const response = await fetch(apiUrl, {
+			method: 'PATCH',
+			body: JSON.stringify(taskData),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} catch (error) {
+		return {
+			error: [{ code: error.code, message: 'Updating task was unsuccessful. API call returned invalid response or application failed to generate API url. [patchTask]' }],
+		};
+	}
 }
 
 /**
@@ -108,34 +200,36 @@ async function patchTask(id, taskData) {
  * @return {Promise} A Promise that resolves to the JSON response from the API
  */
 async function delTask(id) {
-    if ((!Number.isInteger(id) || id < 1)) {
-        throw new Error ("id error (expected Int n > 0)");
-    }
-    try {
-        const apiUrl = generateApiUrl("delete_task_by_id", id);
-        const response  = await fetch(apiUrl, {
-            method: "DELETE"
-        });
-        const jsonResponse = await response.json();
-        return jsonResponse;
-    } catch (error) {
-        return {error: [{code: error.code, message: "Deleting task was unsuccessful. API call returned invalid response or application failed to generate API url."}]}
-    } 
+	if (!Number.isInteger(id) || id < 1) {
+		throw new Error('id error (expected Int n > 0)');
+	}
+	try {
+		const apiUrl = generateApiUrl('delete_task_by_id', id);
+		const response = await fetch(apiUrl, {
+			method: 'DELETE',
+		});
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} catch (error) {
+		return {
+			error: [{ code: error.code, message: 'Deleting task was unsuccessful. API call returned invalid response or application failed to generate API url. [delTask]' }],
+		};
+	}
 }
 
 /**
  * Clears the error message by setting it to an empty string.
  *
  * @param {function} setErrorMessage - a function to set the error message
- * @return {void} 
+ * @return {void}
  */
 function clearErrorMessage(setErrorMessage) {
-    setErrorMessage("");
+	setErrorMessage('');
 }
 
 function generateErrorString(error) {
-    let errorString = error.map(e => e.message).join('<br/>');
-    return errorString;
+	let errorString = error.map((e) => e.message).join('<br/>');
+	return errorString;
 }
 
 /**
@@ -146,23 +240,28 @@ function generateErrorString(error) {
  * @return {string} The generated API URL.
  */
 function generateApiUrl(endpoint, id = 0) {
-    let apiBaseUrl = window.env.REACT_APP_API_URL;
-    let idCondition = (Number.isInteger(id) && id > 0);
-    switch (endpoint) {
-        case "get_tasks":
-        case "post_task":
-            return apiBaseUrl + apiEndpoints[endpoint];
-        case "get_task_by_id":
-        case "patch_task_by_id":
-        case "delete_task_by_id":
-            if (idCondition) {
-                return apiBaseUrl + apiEndpoints[endpoint] + id;
-            }else {
-                throw new Error("generateApiUrl id error (expected Int)");
-            }
-        default:
-            throw new Error("generateApiUrl endpoint error (expected one of [get_tasks, post_task, get_task_by_id, patch_task_by_id, delete_task_by_id])");
-    }
+	let apiBaseUrl = window.env.REACT_APP_API_URL;
+	let idCondition = Number.isInteger(id) && id > 0;
+	switch (endpoint) {
+		case 'get_lists':
+		case 'post_list':
+		case 'get_tasks': //deprecated
+		case 'post_task':
+			return apiBaseUrl + apiEndpoints[endpoint];
+		case 'get_list_tasks':
+		case 'get_task_by_id':
+		case 'patch_task_by_id':
+		case 'delete_task_by_id':
+			if (idCondition) {
+				return apiBaseUrl + apiEndpoints[endpoint] + id;
+			} else {
+				throw new Error('generateApiUrl id error (expected Int)');
+			}
+		default:
+			throw new Error(
+				'generateApiUrl endpoint error (expected one of [get_lists, post_list, get_tasks, post_task, get_list_tasks, get_task_by_id, patch_task_by_id, delete_task_by_id])'
+			);
+	}
 }
 
-export {getTasks, getTaskById, postTask, patchTask, delTask, clearErrorMessage, generateErrorString};
+export { getLists, getListTasks, postList, getTasks, getTaskById, postTask, patchTask, delTask, clearErrorMessage, generateErrorString };
