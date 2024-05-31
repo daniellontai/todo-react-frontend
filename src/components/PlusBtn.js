2731aec98621a8842f6477baeeabbdd4e1b76d01
Lontai Dániel <lontai.daniel@gmail.com>
2024-06-01 00:17:34 +0200

    renamed NewTaskBtn to PlusBtn for reusability

---
+++ src/components/PlusBtn.js
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+/**
+ * Renders a new task button component.
+ *
+ * @param {function} clickHandler - The function to be called when the button is clicked
+ * @return {React.JSX.Element} The new task button component
+ */
+export default function PlusBtn({ clickHandler, className }) {
+	// TODO: rename to PlusBtn or something to reflect reusability (same for className)
+	let classString = 'plus-btn';
+	if (className && className.length > 0) {
+		classString += ' ' + className;
+	}
+	return (
+		<div
+			tabIndex="-1"
+			className={classString}>
+			<button onClick={clickHandler}>
+				<FontAwesomeIcon icon={faPlus} />
+			</button>
+		</div>
+	);
+}
